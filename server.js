9d9852446d29536aff97051648603969b9ea011e
mckshreder <garrettmckenzie@ymail.com>
2015-10-05 00:49:35 -0700

    api/users has -post -put features fully functioning

--- server.js
+++ server.js
@@ -7,48 +7,130 @@ var app = express(); // define our app using express
 var bodyParser = require('body-parser'); // get body-parser
 var morgan = require('morgan'); // used to see requests
 var mongoose = require('mongoose'); // for working w/ our database
+var User = require('./app/models/user');
 var port = process.env.PORT || 8080; // set the port for our app
+// APP CONFIGURATION ---------------------
 
- // APP CONFIGURATION ---------------------
- // use body parser so we can grab information from POST requests
- app.use(bodyParser.urlencoded({ extended: true }));
- app.use(bodyParser.json());
+mongoose.connect('mongodb://mckshreder:password@ds029814.mongolab.com:29814/restfulapi')
 
- // configure our app to handle CORS requests
- app.use(function(req, res, next) {
- res.setHeader('Access-Control-Allow-Origin', '*');
- res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
- res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \ Authorization');
- next();
- });
+// use body parser so we can grab information from POST requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
- // log all requests to the console
- app.use(morgan('dev'));
+// configure our app to handle CORS requests
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \ Authorization');
+	next();
+});
 
- // ROUTES FOR OUR API
- // =============================
+// log all requests to the console
+app.use(morgan('dev'));
+// basic route for the home page
 
- // basic route for the home page
- app.get('/', function(req, res) {
- res.send('Welcome to the home page!');
- });
+app.get('/', function(req, res) {
+	res.send('Welcome to the home page!');
+});
 
- // get an instance of the express router
- var apiRouter = express.Router();
+// =============================
+// ROUTES FOR OUR API
 
- // test route to make sure everything is working
- // accessed at GET http://localhost:8080/api
- apiRouter.get('/', function(req, res) {
- res.json({ message: 'hooray! welcome to our api!' });
- });
 
- // more routes for our API will happen here
+// get an instance of the express router
+var apiRouter = express.Router();
 
- // REGISTER OUR ROUTES -------------------------------
- // all of our routes will be prefixed with /api
- app.use('/api', apiRouter);
+// middleware to use for all requests
+apiRouter.use(function(req, res, next) {
+// do logging
+	console.log('Somebody just came to our app!');
+	// this is where we will authenticate users
+	next(); // make sure we go to the next routes and don't stop here
+});
 
- // START THE SERVER
- // ===============================
- app.listen(port);
- console.log('Magic happens on port ' + port);
+// test route to make sure everything is working
+// accessed at GET http://localhost:8080/api
+apiRouter.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// =============================
+// on routes that end in /users
+apiRouter.route('/users')
+
+	.post(function(req, res) {
+
+		// create a new instance of the User model
+		var user = new User();
+
+		// set the users information (comes from the request)
+		user.name = req.body.name;
+		user.username = req.body.username;
+		user.password = req.body.password;
+
+		// save the user and check for errors
+		user.save(function(err) {
+		if (err) {
+			// duplicate entry
+			 if (err.code == 11000)
+			 return res.json({ success: false, message: 'A user with that username already exists.' });
+			else
+			return res.send(err);
+			}
+			res.json({ message: 'User created!' });
+		});
+	})
+	// get all the users (accessed at GET http://localhost:8080/api/users)
+	.get(function(req, res) {
+	 
+		User.find(function(err, users) {
+			if (err) res.send(err);
+			// return the users
+			res.json(users);
+		});
+	});
+
+//----------------------------------------------------
+// on routes that end in /users/:user_id
+apiRouter.route('/users/:user_id')
+
+	// get the user with that id
+	// (accessed at GET http://localhost:8080/api/users/:user_id)
+	.get(function(req, res) {
+
+		User.findById(req.params.user_id, function(err, user){
+			if (err) res.send(err);
+		// return that user
+		res.json(user);
+		});
+	})
+
+	// update the user with this id
+	// (accessed at PUT http://localhost:8080/api/users/:user_id)
+	.put(function(req, res) {
+
+		// use our user model to find the user we want
+		User.findById(req.params.user_id, function(err, user) {
+			if (err) res.send(err);
+			// update the users info only if its new
+			if (req.body.name) user.name = req.body.name;
+			if (req.body.username) user.username = req.body.username;
+			if (req.body.password) user.password = req.body.password;
+			// save the user
+			user.save(function(err) {
+				if (err) res.send(err);
+				// return a message
+				res.json({ message: 'User updated!' });
+			});
+		});
+	})
+
+//	----------------------------------------------------
+// REGISTER OUR ROUTES
+// all of our routes will be prefixed with /api
+app.use('/api', apiRouter);
+
+// START THE SERVER
+// ===============================
+app.listen(port);
+console.log('Magic happens on port ' + port);
